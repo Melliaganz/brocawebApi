6516d83daa24c1615ae604d54ec6568aef055f0f
Melliaganz <lucaslengranddev@gmail.com>
2026-01-10 01:06:42 +0100

    cloudinary

--- middleware/upload.js
+++ middleware/upload.js
@@ -12,16 +12,14 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'articles',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
-    // On retire le public_id manuel pour laisser Cloudinary générer un ID unique sécurisé
-    // Cela évite les erreurs de signature ("Invalid Signature")
-    transformation: [{ width: 1000, height: 1000, crop: 'limit' }] 
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif']
+    // AUCUNE transformation ici
   },
 });
-console.log("Cloudinary Config Name:", cloudinary.config().cloud_name);
+
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB max
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 module.exports = upload;
